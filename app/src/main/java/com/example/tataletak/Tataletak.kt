5fd1a614927d379d31428cabc0bf32ea253ef3c6
Pradipa Y <merakayamsstraat@gmail.com>
2025-10-16 16:18:22 +0700

    membuat function tataletakBoxcolumnRow

--- app/src/main/java/com/example/tataletak/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/Tataletak.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 
@@ -87,4 +88,9 @@ fun TataletakRowColumn(modifier: Modifier) {
         }
 
     }
+}
+
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier) {
+
 }
